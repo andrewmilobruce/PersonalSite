93c503be1a9b5eff88cb4e2adc324ceb1f3519ec
andrew milo bruce <115047853+andrewmilobruce@users.noreply.github.com>
2023-05-08 11:09:19 -0500

    - Cleaned up code for Personal Site - Added meaningful inline comments, header...

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+/**
+ * Name: index.js
+ * Purpose: Define the interactive behavior for the personal website
+ * Author: Andrew Milo Bruce
+ * Date: May 8th, 2023 (last modified)
+ * Version: 1.0.1
+ * Dependencies: None
+ * 
+ * Description: This file defines the interactive behavior for the personal website, including the dark mode toggle switch,
+ * the smooth scroll behavior for internal links, and any other interactive features.
+ * 
+ * Usage: This file is used to define the interactive behavior for the HTML elements in the personal website.
+ * 
+ * Modifications:
+ *  - May 8th, 2023 - cleaned up code and added meaningful inline comments, header...
+ * 
+ * Notes: None
+ */
+
+// Get the current date and time
 var today = new Date();
+
+// Get the HTML elements we will be manipulating
 var bg = document.getElementById("dark-mode-bg");
 var words = document.getElementsByClassName("dark-mode-word");
 var nav = document.getElementById("dark-mode-nav");
 
+// Print the current hour to the console for debugging purposes
 console.log(today.getHours());
 
+// If it's later than 5 PM (17:00), change the background color, text color, and navbar color to dark mode
 if (today.getHours() > 17) {
     bg.style.backgroundColor = "#4C6079";
     for (let word of words) {
@@ -15,4 +39,3 @@ if (today.getHours() > 17) {
     nav.classList.add("navbar-dark")
     nav.classList.add("bg-dark")
 }
-
